121818762859d2a06b69b53c8192f7cd3c04de64
Tymur Kosiak <us1gav@gmail.com>
2023-08-15 23:45:24 +0300

    Update homework lesson 4. The static implementation of the animal counter has been replaced with a non-static one.

--- src/main/java/edu/hillel/homework/lesson4/Animals.java
+++ src/main/java/edu/hillel/homework/lesson4/Animals.java
@@ -2,17 +2,11 @@ package edu.hillel.homework.lesson4;
 
 public class Animals {
 
-    //private static int totalAnimalsCount = 0;
     private String name;
 
     public Animals(String name) {
         this.name = name;
-        //totalAnimalsCount++;
     }
-
-    //public static int getTotalAnimalsCount() {
-    //    return totalAnimalsCount;
-    //}
 
     public String getName() {
         return name;
