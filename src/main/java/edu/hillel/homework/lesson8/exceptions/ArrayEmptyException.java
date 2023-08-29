23d51a6e0e435132d4455e3d4ea7e23c1cd53881
Tymur Kosiak <us1gav@gmail.com>
2023-08-29 22:33:16 +0300

    Added homework lesson 8.

---
+++ src/main/java/edu/hillel/homework/lesson8/exceptions/ArrayEmptyException.java
@@ -0,0 +1,8 @@
+package edu.hillel.homework.lesson8.exceptions;
+
+public class ArrayEmptyException extends Exception {
+
+    public ArrayEmptyException(String message) {
+        super(message);
+    }
+}
