b28d785b34d7f5ab30033e28e56b5cc638e3d41d
Tymur Kosiak <us1gav@gmail.com>
2023-08-30 19:03:17 +0300

    Updated homework lesson 8.

--- src/main/java/edu/hillel/homework/lesson8/exceptions/ArrayDataException.java
+++ src/main/java/edu/hillel/homework/lesson8/exceptions/ArrayDataException.java
@@ -2,7 +2,7 @@ package edu.hillel.homework.lesson8.exceptions;
 
 public class ArrayDataException extends Exception {
 
-    public ArrayDataException(String message, Throwable stackTrace) {
-        super(message, stackTrace);
+    public ArrayDataException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
